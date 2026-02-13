695cc181ede211a638647eefc146c6031bfecfb2
Sean Bodnar <seanbodnar@example.com>
2026-02-13 00:40:36 -0500

    new push

--- .api/routers.js
+++ .api/routers.js
@@ -2,19 +2,13 @@
 // Files Imports
 import * as configure from "@api/configure";
 import * as API_000 from "@api/root/api/commerce/create-checkout-session/POST.ts";
-import * as API_001 from "@api/root/api/contact/POST.ts";
-import * as API_002 from "@api/root/api/health/GET.ts";
 
 // Public RESTful API Methods and Paths
 // This section describes the available HTTP methods and their corresponding endpoints (paths).
 // POST /api/commerce/create-checkout-session/    api/commerce/create-checkout-session/POST.ts
-// POST /api/contact/                             api/contact/POST.ts
-// GET  /api/health/                              api/health/GET.ts
 
 const internal  = [
-  API_000.default  && { cb: API_000.default , method: "post" , route: "/commerce/create-checkout-session/" , url: "/api/commerce/create-checkout-session/" , source: "api/commerce/create-checkout-session/POST.ts" },
-  API_001.default  && { cb: API_001.default , method: "post" , route: "/contact/"                          , url: "/api/contact/"                          , source: "api/contact/POST.ts"                          },
-  API_002.default  && { cb: API_002.default , method: "get"  , route: "/health/"                           , url: "/api/health/"                           , source: "api/health/GET.ts"                            }
+  API_000.default  && { cb: API_000.default , method: "post" , route: "/commerce/create-checkout-session/" , url: "/api/commerce/create-checkout-session/" , source: "api/commerce/create-checkout-session/POST.ts" }
 ].filter(it => it);
 
 export const routers = internal.map((it) => {
