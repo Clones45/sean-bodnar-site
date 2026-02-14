edf44344f1b1dac8e73094361c507e606a41a9f0
Sean Bodnar <seanbodnar@example.com>
2026-02-14 09:33:33 -0500

    correected sitemap

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -10,6 +10,31 @@ export default function AboutPage() {
                 name="description"
                 content="Meet Sean, your trusted real estate professional in Eldersburg, MD. Learn about my experience, local expertise, and commitment to helping you find your dream home."
             />
+            <script type="application/ld+json">
+                {JSON.stringify({
+                    "@context": "https://schema.org",
+                    "@type": "RealEstateAgent",
+                    "name": "Sean Bodnar - Keller Williams Lucido Agency",
+                    "image": "https://img1.wsimg.com/isteam/ip/daf89318-8ad7-4a11-8f4a-b5842fa45c45/401845630_311528424972365_6482679109257298931_.jpg",
+                    "url": "https://sellingwithsean.com/about/",
+                    "telephone": "+14109403032",
+                    "priceRange": "$$$",
+                    "address": {
+                        "@type": "PostalAddress",
+                        "streetAddress": "6229 Sykesville Rd",
+                        "addressLocality": "Eldersburg",
+                        "addressRegion": "MD",
+                        "postalCode": "21784",
+                        "addressCountry": "US"
+                    },
+                    "areaServed": [
+                        "Eldersburg",
+                        "Sykesville",
+                        "Carroll County",
+                        "Howard County"
+                    ]
+                })}
+            </script>
 
             <div className="min-h-screen">
                 {/* Hero Section */}
