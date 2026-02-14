e0a0d82fa29c221660fa15049dee04d7049d2246
Sean Bodnar <seanbodnar@example.com>
2026-02-14 09:46:08 -0500

    new sitemaps

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -16,7 +16,7 @@ export default function AboutPage() {
                     "@type": "RealEstateAgent",
                     "name": "Sean Bodnar - Keller Williams Lucido Agency",
                     "image": "https://img1.wsimg.com/isteam/ip/daf89318-8ad7-4a11-8f4a-b5842fa45c45/401845630_311528424972365_6482679109257298931_.jpg",
-                    "url": "https://sellingwithsean.com/about/",
+                    "url": "https://www.sellingwithsean.com/about/",
                     "telephone": "+14109403032",
                     "priceRange": "$$$",
                     "address": {
